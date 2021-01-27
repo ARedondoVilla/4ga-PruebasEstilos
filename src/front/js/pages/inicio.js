ac89f27f22a48f06a77b3e61ed6d06e4d7436a2c
ARedondoVilla <alexredondovilla@gmail.com>
2021-01-27 11:00:19 +0000

    Tamaños de row y col-4 en px

--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -7,14 +7,25 @@ export const Inicio = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col">
+				<div className="col text-center">
 					<h1>Aplicando estilos aqui....a ver que cojones sale!!!!</h1>
 				</div>
 			</div>
-			<div className="row">
-				<div className="col size-img">
-					<img src="https://picsum.photos/400/400" />
+			<div className="row align-items-center">
+				<div className="col-4 size-img text-center">
+					<img src="https://picsum.photos/1080/1080" />
 				</div>
+				<div className="col-4 text-center">
+					<h3>Alejandro Redondo Villa</h3>
+				</div>
+				<div className="col-4 text-center">
+					<h3>Otro texto random</h3>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-4 box py-3" />
+				<div className="col-4 box" />
+				<div className="col-4 box" />
 			</div>
 		</div>
 	);
