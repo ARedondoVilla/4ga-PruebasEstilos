424eac96415f7f5a9042bcb7ea12294a246578ba
ARedondoVilla <alexredondovilla@gmail.com>
2021-01-30 12:59:39 +0000

    prueba elementos responsive

--- src/front/js/pages/test.js
+++ src/front/js/pages/test.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Modal } from "../component/modal";
+import { Link } from "react-router-dom";
 
 export const Test = () => {
 	const { store, actions } = useContext(Context);
@@ -10,20 +11,35 @@ export const Test = () => {
 
 	return (
 		<div className="container">
-			<div className="row">
-				<div className="col text-center">
-					<h1>Prueba del modal</h1>
+			<div className="row mt-4">
+				<div className="col-xl-4 my-1">
+					<div className="row box-card-user mx-1">
+						<h3>perfil usuario</h3>
+					</div>
+				</div>
+				<div className="col-xl-8 my-1">
+					<div className="row box-card-post mx-1 align-items-center">
+						<div className="col-lg-4 text-center img-post px-2 py-2">
+							<img src="https://picsum.photos/800/800" alt="imagen" />
+						</div>
+						<div className="col-lg-8 px-2 py-2">
+							<span className="text-muted">26 Diciembre 2020</span>
+							<h3>Lorem Ipsum</h3>
+							<p>
+								Lorem fistrum qué dise usteer no te digo trigo por no llamarte Rodrigor a wan.
+								Benemeritaar ese que llega ese hombree tiene musho peligro apetecan no puedor diodeno.
+								Mamaar ahorarr fistro ese que llega diodenoo caballo blanco caballo negroorl te va a
+								hasé pupitaa está la cosa muy malar.
+							</p>
+							<Link to="/inicio">
+								<p type="button" className="button-post">
+									Leer mas
+								</p>
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
-			<button
-				type="button"
-				className="btn btn-primary"
-				data-toggle="modal"
-				data-target="#exampleModal"
-				onClick={() => setState({ showModal: true })}>
-				Launch demo modal
-			</button>
-			<Modal show={state.showModal} />
 		</div>
 	);
 };
