6d690897b049019be63110f082be2a8daa5506d7
ARedondoVilla <alexredondovilla@gmail.com>
2021-02-11 20:14:33 +0000

    plantilla estructura

--- src/front/js/pages/option.js
+++ src/front/js/pages/option.js
@@ -8,44 +8,29 @@ import { Card } from "../component/card";
 export const Option = () => {
 	const { store, actions } = useContext(Context);
 
-	const showPost = store.userPost.map((post, index) => {
-		return (
-			<div key={index}>
-				<Card item={post} />
-			</div>
-		);
-	});
-
 	return (
-		<>
-			<div className="container">
-				<div className="row">
-					<div className="col-12">
-						<h1>Titulo</h1>
-					</div>
-					<div className="col-12">
-						<h3>Subtitulo</h3>
-					</div>
-					<div className="col-12">
-						<div className="row justify-content-center">
-							<div className="col-9">
-								<p>
-									Lorem fistrum ese pedazo de no te digo trigo por no llamarte Rodrigor de la pradera
-									amatomaa la caidita a wan de la pradera sexuarl condemor condemor. Te va a hasé
-									pupitaa diodenoo diodeno ahorarr amatomaa se calle ustée torpedo la caidita hasta
-									luego Lucas. Ahorarr papaar papaar no te digo trigo por no llamarte Rodrigor
-									diodenoo a gramenawer qué dise usteer amatomaa. Por la gloria de mi madre a wan
-									fistro qué dise usteer benemeritaar te voy a borrar el cerito diodeno no te digo
-									trigo por no llamarte Rodrigor condemor. Fistro está la cosa muy malar pupita
-									fistro. Pupita tiene musho peligro quietooor de la pradera pecador te voy a borrar
-									el cerito caballo blanco caballo negroorl te voy a borrar el cerito.
-								</p>
-							</div>
-						</div>
-					</div>
+		<div className="container">
+			<div className="row">
+				<div className="col-4">
+					<h3>COLUM 4</h3>
+				</div>
+				<div className="col-8">
+					<h3>COLUM 8</h3>
 				</div>
 			</div>
-			<div className="fluid-container full-width-color" />
-		</>
+			<div className="row">
+				<div className="col-12">
+					<hr className="line-divisor" />
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-4">
+					<h3>COLUM 4</h3>
+				</div>
+				<div className="col-8">
+					<h3>COLUM 8</h3>
+				</div>
+			</div>
+		</div>
 	);
 };
